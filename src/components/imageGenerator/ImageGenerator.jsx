6045b59885078ef1079192baec3cff80a20521cd
AneeshBist <aneeshbist@gmail.com>
2023-10-05 22:06:52 +0530

    adding css

--- src/components/imageGenerator/ImageGenerator.jsx
+++ src/components/imageGenerator/ImageGenerator.jsx
@@ -5,9 +5,13 @@ import defaultImg from '../assets/img-default.jpg'
 const ImageGenerator = () => {
   return (
     <div className='ai-image-generator'>
-        <div className="header">AI Image Generator</div>
+        <h1 className="header">AI Image  <span>Generator</span></h1>
         <div className="img-loading">
             <div className="img"><img src={defaultImg} alt="default-image" /></div>
+        </div>
+        <div className="search-box">
+            <input type="text" className='search-input' placeholder='Description Here...'/>
+            <div className="generate-btn">Generate </div>
         </div>
     </div>
   )
